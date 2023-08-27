afcf3b078800b07487cbac8ca2e47bd05a15ce60
hizrian <hizzrian@gmail.com>
2023-08-28 00:48:59 +0700

    first web3

---
+++ src/App.tsx
@@ -0,0 +1,115 @@
+import './App.css'
+import { useState, useEffect } from 'react'
+import { formatBalance, formatChainAsNum } from './utils'
+import detectEthereumProvider from '@metamask/detect-provider'
+
+const App = () => {
+  const [hasProvider, setHasProvider] = useState<boolean | null>(null)
+  const initialState = { accounts: [], balance: "", chainId: "" }
+  const [wallet, setWallet] = useState(initialState)
+
+  const [isConnecting, setIsConnecting] = useState(false)  /* New */
+  const [error, setError] = useState(false)                /* New */
+  const [errorMessage, setErrorMessage] = useState("")     /* New */
+
+  useEffect(() => {
+    const refreshAccounts = (accounts: any) => {
+      if (accounts.length > 0) {
+        updateWallet(accounts)
+      } else {
+        // if length 0, user is disconnected
+        setWallet(initialState)
+      }
+    }
+
+    const refreshChain = (chainId: any) => {
+      setWallet((wallet) => ({ ...wallet, chainId }))
+    }
+
+    const getProvider = async () => {
+      const provider = await detectEthereumProvider({ silent: true })
+      setHasProvider(Boolean(provider))
+
+      if (provider) {
+        const accounts = await window.ethereum.request(
+          { method: 'eth_accounts' }
+        )
+        refreshAccounts(accounts)
+        window.ethereum.on('accountsChanged', refreshAccounts)
+        window.ethereum.on("chainChanged", refreshChain)
+      }
+    }
+
+    getProvider()
+
+    return () => {
+      window.ethereum?.removeListener('accountsChanged', refreshAccounts)
+      window.ethereum?.removeListener("chainChanged", refreshChain)
+    }
+  }, [])
+
+  const updateWallet = async (accounts: any) => {
+    const balance = formatBalance(await window.ethereum!.request({
+      method: "eth_getBalance",
+      params: [accounts[0], "latest"],
+    }))
+    const chainId = await window.ethereum!.request({
+      method: "eth_chainId",
+    })
+    setWallet({ accounts, balance, chainId })
+  }
+
+  const handleConnect = async () => {                   /* Updated */
+    setIsConnecting(true)                               /* New */
+    await window.ethereum.request({                     /* Updated */
+      method: "eth_requestAccounts",
+    })
+    .then((accounts:[]) => {                            /* New */
+      setError(false)                                   /* New */
+      updateWallet(accounts)                            /* New */
+    })                                                  /* New */
+    .catch((err:any) => {                               /* New */
+      setError(true)                                    /* New */
+      setErrorMessage(err.message)                      /* New */
+    })                                                  /* New */
+    setIsConnecting(false)                              /* New */
+  }
+
+  const disableConnect = Boolean(wallet) && isConnecting
+
+  return (
+    <div className="App">
+      {/* set center layout */}
+      <div className="overflow-hidden rounded-lg bg-gray-100 p-4 flex justify-center">
+       <div className="px-4 py-5">
+      {/* set background */}
+      
+      {window.ethereum?.isMetaMask && wallet.accounts.length < 1 &&
+                /* Updated */
+        <button className="inline-flex items-center rounded-full border border-transparent bg-indigo-600 p-1 text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2" disabled={disableConnect} onClick={handleConnect}>Connect MetaMask</button>
+      }
+
+      {wallet.accounts.length > 0 &&
+        <>
+        {/* card using tailwind */}
+          
+            <h1 className="text-3xl font-bold underline">Wallet Connected</h1>
+            <div>Wallet Accounts: {wallet.accounts[0]}</div>
+            <div>Wallet Balance: {wallet.balance}</div>
+            <div>Hex ChainId: {wallet.chainId}</div>
+            <div>Numeric ChainId: {formatChainAsNum(wallet.chainId)}</div>
+        </>
+      }
+      { error && (                                        /* New code block */
+          <div onClick={() => setError(false)}>
+            <strong>Error:</strong> {errorMessage}
+          </div>
+        )
+      }
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
